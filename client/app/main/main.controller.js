8b33f819f1ed99ba08f3840c304fc384e885d175
Katherine Van Winkle <katherinevanwinkle@new-host.home>
2015-06-29 18:44:14 -0400

    implemented webcam

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -19,10 +19,60 @@ angular.module('greenOutYourClosetApp')
 
    }
 
-   $scope.takeSnapshot = function(){
-    console.log("second function worked")
+   angular.element(document).ready(function () {
+
     
-   }
+    console.log("second function worked")
+
+    var photoButton = document.getElementById('snapPicture');
+photoButton.addEventListener('click', picCapture, false);
+
+navigator.getUserMedia ||
+  (navigator.getUserMedia = navigator.mozGetUserMedia ||
+  navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+  
+if (navigator.getUserMedia){
+    navigator.getUserMedia({video:true,audio:false}, onSuccess, onError);
+  }else{
+    alert('Your browser is not supported');
+}
+
+function onSuccess(stream){
+  var vidContainer = document.getElementById('webcam');
+  var vidStream;
+  
+  if (window.URL){
+      vidStream = window.URL.createObjectURL(stream);
+    }else{
+    vidStream = stream;
+  }
+  
+  vidContainer.autoplay = true;
+  vidContainer.src = vidStream;
+}
+
+function onError(){
+  alert('Houston, we have a problem');
+}
+
+function picCapture(){
+
+  var vidContainer = document.getElementById('webcam');
+  var picture = document.getElementById('capture'),
+    context = picture.getContext('2d');
+  
+    
+  picture.width = "600";
+  picture.height = "400";
+  
+  context.drawImage(vidContainer, 0, 0, picture.width, picture.height);
+  var dataURL = picture.toDataURL();
+  document.getElementById('canvasImg').src = dataURL;
+}
+
+
+    
+   });
 
 
   });
